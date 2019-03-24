71493fbf10b8300bef9cd58fcf39602e22958288
brent groves <brent@localhost.localdomain>
2019-03-23 20:07:16 -0400

    Init

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,83 @@
+package hello;
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+public class HelloWorld {
+ 
+    public static void main(String[] args) {
+ 
+        // variables
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+ 
+        // Step 1: Loading or 
+        // registering Oracle JDBC driver class
+        try {
+ 
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+        }
+        catch(ClassNotFoundException cnfex) {
+ 
+            System.out.println("Problem in loading or "
+                    + "registering MS Access JDBC driver");
+            cnfex.printStackTrace();
+        }
+ 
+        // Step 2: Opening database connection
+        try {
+ 
+            String msAccDB = "/home/brent/src/Northwind.MDB";
+            String dbURL = "jdbc:ucanaccess://"
+                    + msAccDB; 
+	    int result;
+ 
+            // Step 2.A: Create and 
+            // get connection using DriverManager class
+            connection = DriverManager.getConnection(dbURL); 
+ 
+            // Step 2.B: Creating JDBC Statement 
+            statement = connection.createStatement();
+ 
+            // Step 2.C: Executing SQL and 
+            // retrieve data into ResultSet
+	    result = statement.executeUpdate("UPDATE Customers SET ContactName = 'sammy' WHERE CustomerID = '1'");	    
+//            resultSet = statement.executeQuery("SELECT * FROM customers");
+ 
+            System.out.println("ID\tName\t\t\tAge\tMatches");
+            System.out.println("==\t================\t===\t=======");
+/* 
+            // processing returned data and printing into console
+            while(resultSet.next()) {
+                System.out.println(resultSet.getString(1) + "\t" + 
+                        resultSet.getString(2) + "\t" + 
+                        resultSet.getString(3) + "\t" +
+                        resultSet.getString(4));
+            }
+*/	    
+        }
+        catch(SQLException sqlex){
+            sqlex.printStackTrace();
+        }
+        finally {
+            // Step 3: Closing database connection
+            try {
+                if(null != connection) {
+                    // cleanup resources, once after processing
+                  //  resultSet.close();
+                    statement.close();
+ 
+                    // and then finally close connection
+                    connection.close();
+                }
+            }
+            catch (SQLException sqlex) {
+                sqlex.printStackTrace();
+            }
+        }
+    }
+}
